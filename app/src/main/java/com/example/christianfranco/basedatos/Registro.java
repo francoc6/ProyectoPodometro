6a831f84dd6bc34bba0a0197e97d8aec4f18fa45
francoc6 <44350586+francoc6@users.noreply.github.com>
2018-10-28 00:45:55 -0500

    agrege pantalla principal

--- app/src/main/java/com/example/christianfranco/basedatos/Registro.java
+++ app/src/main/java/com/example/christianfranco/basedatos/Registro.java
@@ -2,6 +2,7 @@ package com.example.christianfranco.basedatos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -23,8 +24,11 @@ public class Registro extends AppCompatActivity {
         Usuario = (EditText)findViewById(R.id.Usuario);
         Correo = (EditText)findViewById(R.id.Correo);
         Edad = (EditText)findViewById(R.id.Edad);
+        Genero=(Spinner)findViewById(R.id.spinnerGenero);
         Cancelar =(Button)findViewById(R.id.BotonCancelar);
         Registrar=(Button)findViewById(R.id.BotonAgregar);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.genero, android.R.layout.simple_spinner_item);
+        Genero.setAdapter(adapter);
 
 
     }
