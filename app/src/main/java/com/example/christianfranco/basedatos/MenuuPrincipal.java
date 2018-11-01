93d2bc38c9909beaf343dd3b5e43f4bbe5cd3b51
francoc6 <44350586+francoc6@users.noreply.github.com>
2018-11-01 17:18:40 -0500

    clima

--- app/src/main/java/com/example/christianfranco/basedatos/MenuuPrincipal.java
+++ app/src/main/java/com/example/christianfranco/basedatos/MenuuPrincipal.java
@@ -1,21 +1,17 @@
 package com.example.christianfranco.basedatos;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 public class MenuuPrincipal extends AppCompatActivity {
-
     Button btnIngresar;
     Button btnEsta;
     Button btnReloj;
     Button btnDatos;
     Button btnLogOut;
     SharedPreferences sp;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +22,6 @@ public class MenuuPrincipal extends AppCompatActivity {
         btnDatos =(Button)findViewById(R.id.btnDatos) ;
         btnEsta =(Button)findViewById(R.id.btnEsta) ;
         btnIngresar =(Button)findViewById(R.id.btnIN);
-
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,9 +31,6 @@ public class MenuuPrincipal extends AppCompatActivity {
                 finish();//evito que regresen al presionar el boto del celular
             }
         });
-
-
-
         btnDatos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,7 +38,5 @@ public class MenuuPrincipal extends AppCompatActivity {
                 startActivity(actividades);
             }
         });
-
-
     }
 }
