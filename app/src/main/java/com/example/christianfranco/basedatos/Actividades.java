830a965d34ef3b3b5f33ec371052c726dad8f2ec
francoc6 <44350586+francoc6@users.noreply.github.com>
2018-11-01 16:27:17 -0500

    Registros

---
+++ app/src/main/java/com/example/christianfranco/basedatos/Actividades.java
@@ -0,0 +1,15 @@
+package com.example.christianfranco.basedatos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Actividades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividades);
+
+
+    }
+}
