d480f12117a45ddb10a6ac3f56c58d488be1dd6a
francoc6 <44350586+francoc6@users.noreply.github.com>
2018-11-10 01:04:43 -0500

    Agrego cronometro

--- app/src/main/java/com/example/christianfranco/basedatos/Actividad.java
+++ app/src/main/java/com/example/christianfranco/basedatos/Actividad.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 import com.example.christianfranco.basedatos.ContadordePasos.IntSerBack;
@@ -14,17 +15,40 @@ public class Actividad extends AppCompatActivity {
     public static TextView TvSteps;
     private static final String TEXT_NUM_STEPS = "Numero de pasos  realizados: ";
     Button BtnStart, BtnStop;
+    Chronometer reloj ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad);
+
+        reloj = (Chronometer)findViewById(R.id.simpleChronometer);
+
         TvSteps = (TextView) findViewById(R.id.tv_steps);
         BtnStart = (Button) findViewById(R.id.btn_start);
         BtnStop = (Button) findViewById(R.id.btn_stop);
         TvSteps.setText(TEXT_NUM_STEPS + IntSerBack.getNumSteps());//obtengo los pasos dados, para que aparezca al iniciar
 
+        BtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reloj.start();
+                reloj.setFormat("Tiempo:  %s");
+            }
+        });
 
+
+        BtnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reloj.stop();
+
+            }
+        });
 
     }
+
+
+
+
 }
