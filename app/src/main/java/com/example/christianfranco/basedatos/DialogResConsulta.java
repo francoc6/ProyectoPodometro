b6986a8de71526e014e7ed49c9a640f66487b166
francoc6 <44350586+francoc6@users.noreply.github.com>
2018-11-27 14:09:46 -0500

    se agrego dialog resde de consulta

--- app/src/main/java/com/example/christianfranco/basedatos/DialogResConsulta.java
+++ app/src/main/java/com/example/christianfranco/basedatos/DialogResConsulta.java
@@ -1,7 +1,24 @@
 package com.example.christianfranco.basedatos;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class DialogResConsulta extends DialogFragment {
+
+    @Nullable
+    public void onCreate(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_resconsulta, container, false);
+
+
+
+
+    }
+
+
+
 
 }
