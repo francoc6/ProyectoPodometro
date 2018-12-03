d751cea43f950a8fcc1c3cbdd3e442f019797d5e
francoc6 <44350586+francoc6@users.noreply.github.com>
2018-12-02 22:15:21 -0500

    Status  empieza a mostrar resultados desde base

--- app/src/main/java/com/example/christianfranco/basedatos/Status.java
+++ app/src/main/java/com/example/christianfranco/basedatos/Status.java
@@ -34,21 +34,16 @@ public class Status extends AppCompatActivity {
     }
 
 
-
+//obtengo los ultimos valores de las variables del usuario
     Conectar conectar = new Conectar();
-    public ArrayList<String> obtenerdatos(String u){
+    public void obtenerdatos(String u){
         ArrayList<String> resul=new ArrayList<>();
         ArrayList<String> temp=new ArrayList<>();
         try {
             Statement pedir = conectar.conectarabase().createStatement();
-          //  String orden1 = "select Valor from Variables_db where Usuario='"+u+"' AND Id='1'";
-            //String orden2 = "select Valor from Variables_db where Usuario='"+u+"' AND Id='2'";
-            //String orden3 = "select Valor from Variables_db where Usuario='"+u+"' AND Id='3'";
             ResultSet res = null;
             for (int x=1;x<4;x++){
-               // res = pedir.executeQuery("orden"+x);
                 res = pedir.executeQuery("select Valor from Variables_db where Usuario='"+u+"' AND Id='"+x+"'");
-                // res.next();
                 while (res.next()) {
                     temp.add(res.getString("Valor"));
                 }
@@ -60,7 +55,6 @@ public class Status extends AppCompatActivity {
                 }
                 temp.clear();
             }
-            // res.next();
             res.close();
             Psangre.setText(resul.get(0));
             Ppresion.setText(resul.get(1));
@@ -68,9 +62,5 @@ public class Status extends AppCompatActivity {
         } catch (SQLException e) {
             Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-
-
-
-        return resul;
     }
 }
