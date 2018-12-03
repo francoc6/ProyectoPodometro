159afea60e06c817f3757f69e4914592aa903210
francoc6 <44350586+francoc6@users.noreply.github.com>
2018-12-03 00:02:12 -0500

    Status  empieza a mostrar resultados desde base

---
+++ app/src/main/java/com/example/christianfranco/basedatos/Logros.java
@@ -0,0 +1,13 @@
+package com.example.christianfranco.basedatos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Logros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logros);
+    }
+}
